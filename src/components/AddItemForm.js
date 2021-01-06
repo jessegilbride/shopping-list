3fa6639a383866d3b6b3078a7891dcffc79dbe33
Jesse Gilbride <jessegilbride.dev@gmail.com>
2021-01-06 17:26:49 -0500

    created components and their test files

---
+++ src/components/AddItemForm.js
@@ -0,0 +1,15 @@
+export default class AddItemForm extends React.Component {
+  render() {
+    /* TODO: need to wire up the form to add items in App state... */
+    return (
+      <form>
+        <input
+          type='text'
+          placeholder='carrots'
+          aria-label='Shopping list item'
+        />
+        <button type='submit'>Add Item</button>
+      </form>
+    )
+  }
+}
